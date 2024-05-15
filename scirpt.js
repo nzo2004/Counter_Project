ef2fe6cf87b18bbdf485288fe02b8d7c661702f8
nzo2004 <111644119+nzo2004@users.noreply.github.com>
2024-05-15 22:40:25 +0300

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -1,21 +1,15 @@
-//Oradaki elemanları id olarak çağrırız
 let buttonIncerase=document.getElementById('increase');
 let buttonDecrease=document.getElementById('decrease');
 let buttonReset=document.getElementById('reset');
 let counterShow=document.getElementById('counter');
 
-//ordaki sayı çağrırız
 let count=0;
 
-//addEventListener = tıklayınca ne olacağına söyleriz
-
-//Artırma işlemini bu fonksiyonu ile yapıyoruz
 buttonIncerase.addEventListener('click',()=>{
     count+=1;
     counterShow.innerHTML=count;
 })
 
-//Azaltma işlemini bu fonksiyonu ile yapıyoruz
 buttonDecrease.addEventListener('click',()=>{
     if(count==0){
         
@@ -25,7 +19,6 @@ buttonDecrease.addEventListener('click',()=>{
     }
 })
 
-//Resetleme işlemini bu fonksiyonu ile yapıyoruz
 buttonReset.addEventListener('click',()=>{
     if(confirm('Resetlemek istiyor musunuz?')==true){
         count=0;
